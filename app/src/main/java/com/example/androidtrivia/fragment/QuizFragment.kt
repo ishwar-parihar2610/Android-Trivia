e9e6962e2958f4b37c8c0399740b3f3bed031904
ishwar parihar <paruharishwer26102000@gmail.com>
2021-12-26 15:25:31 +0530

    hello

--- app/src/main/java/com/example/androidtrivia/fragment/QuizFragment.kt
+++ app/src/main/java/com/example/androidtrivia/fragment/QuizFragment.kt
@@ -12,6 +12,54 @@ import com.example.androidtrivia.databinding.FragmentQuizBinding
 
 
 class QuizFragment : Fragment() {
+
+    data class Questions(
+        val text: String,
+        val answer: List<String>
+
+
+    )
+
+    private val questions: MutableList<Questions> = mutableListOf(
+        Questions(
+            text = "What is Out Country Name",
+            answer = listOf("India", "America", "China", "Japan")
+
+        ),
+
+        Questions(
+            text = "What is Capital Of India",
+            answer = listOf("Delhi", "Jaipur", "Jodhpur", "Mumbai")
+
+        ),
+
+        Questions(
+            text = "How many state in India",
+            answer = listOf("29", "40", "28", "44")
+
+        ),
+
+        Questions(
+            text = "How many district in Rajasthan",
+            answer = listOf("33", "40", "50", "50")
+
+        ),
+
+        Questions(
+            text = "Who's the father Of nation",
+            answer = listOf("Mahatma Gandhi", "Narendra Modi", "Apj Abdul kalam", "Rahul Gandhi")
+
+        ),
+
+
+        )
+
+    lateinit var currentQuestions: Questions;
+    lateinit var answers: MutableList<String>;
+    private var questionsIndex = 0;
+    private var totalQuestions = 5;
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,14 +71,46 @@ class QuizFragment : Fragment() {
             container,
             false
         );
-        binding.overBtn.setOnClickListener {
-            it.findNavController().navigate(R.id.action_quizFragment_to_quizOverFragment);
-        }
+        binding.questions = this
+        randomizeQuestions();
+        randomizeQuestions()
+        binding.submitBtn.setOnClickListener {
+            val checkId=binding.radioGroup.checkedRadioButtonId;
+            if (checkId!=-1){
+                var answerIndex=0
+                when(checkId){
+                    R.id.answerSecond->answerIndex=1
+                    R.id.answerThird->answerIndex=2
+                    R.id.answerFourth->answerIndex=3
+                }
 
-        binding.winBtn.setOnClickListener {
-            it.findNavController().navigate(R.id.action_quizFragment_to_quizWinFragment);
+                if (answers[answerIndex]==currentQuestions.answer[0]){
+                    questionsIndex++;
+                    if (questionsIndex<totalQuestions){
+                        currentQuestions=questions[questionsIndex]
+                        setQuestion()
+                        binding.invalidateAll()
+                    }else{
+                        it.findNavController().navigate(R.id.action_quizFragment_to_quizWinFragment)
+                    }
+                }else {
+                    it.findNavController().navigate(R.id.action_quizFragment_to_quizOverFragment)
+                }
+            }
         }
         return binding.root;
+    }
+
+    private fun randomizeQuestions() {
+        questions.shuffle();
+        questionsIndex = 0;
+        setQuestion();
+    }
+
+    private fun setQuestion() {
+        currentQuestions = questions[questionsIndex]
+        answers = currentQuestions.answer.toMutableList()
+        answers.shuffle();
     }
 
 
